bf7f42f01a43fc9b6753b29e7e0e44ecfcf3844c
Ivo Arrey <arreyves@gmail.com>
2022-08-31 17:13:19 +0000

    updated data and analysis code

--- Code/raw_code/tidy0.R
+++ Code/raw_code/tidy0.R
@@ -1,6 +1,65 @@
 library(tidyverse)
 library(readxl)
 library(plyr)
+library(fs)
+
+
+# read the raw data with all sites combined
+x1_data <- "/cloud/project/Data/raw_data/CCBOR Water Testing Project data.xlsx"
+ccbor_raw_data <- read_excel('CCBOR Water Testing Project data.xlsx')
+
+# Before reading the data, we will return the names of all sheets containing 
+# the site number for later use
+
+excel_sheets(x1_data)  
+
+# We can now read in just each site from the spreadsheet by specifying the
+# the sheet name or number
+
+df_Site_name <- read_excel(path = x1_data, sheet = "Site 1")  # or
+
+df_Site_number <- read_excel(path = x1_data, sheet = 1)
+
+identical(df_Site_name, df_Site_number)  # test
+
+
+# write a function to read each tab and clean it up before append into a list
+
+
+all_sites <- function(k) {
+  
+  # read data & excel sheet number 
+  xl_data <- "/cloud/project/Data/raw_data/CCBOR Water Testing Project data.xlsx"
+  df_Site_number <- read_excel(path = x1_data, sheet = k)
+  
+   
+    # Time series plot
+    
+    df_Site_number %>% mutate(date = as.Date(Sampling_date, "%m/%d/%y"), Site = as.factor(`Site(1-9)`)) %>% 
+      ggplot(aes(date, `Conductivity(µS)`, colour=Site)) + geom_line(na.rm = T) + 
+      labs(y='Conductivity(µS)')
+    
+    # Spatial Plot
+    
+      
+      
+       }
+    
+############  
+
+# Time series plot  
+
+ccbor_raw_data <- read_excel(path = x1_data, sheet = 10)
+ccbor_raw_data %>% mutate(date = as.Date(Sampling_date, "%m/%d/%y"), Site = as.factor(`Site(1-9)`)) %>% 
+     ggplot(aes(date, `Conductivity(µS)`, colour=Site)) + geom_line(na.rm = T) + 
+     labs(y='Conductivity(µS)')
+
+# Spatial plot
+
+
+      
+
+
 
 combined_data <- read.csv("Data/tidy_data/ccbordata.csv", stringsAsFactors = T)
 sampling_date1 <- as.Date(combined_data$Sampling_date, "%m/%d/%y")
--- Code/raw_code/tidy_data.R
+++ Code/raw_code/tidy_data.R
@@ -21,14 +21,29 @@ df_Site_number <- read_excel(path = x1_data, sheet = 1)
 identical(df_Site_name, df_Site_number)  # test
 
 
-# To import all sheets from a workbook. We will do this via lapply(), 
+# To import all sheets from a workbook. We will do this via lapply() and do.call(), 
 # iterating over the names (or range) of our sheets; passing read_excel() as 
 # our function. The resulting object should be a list of nine (9) data frames; 
 # one (1) per tab.
 
 tab_names <- excel_sheets(path = x1_data)
 
-list_all <- lapply(tab_names, function(x) read_excel(path = x1_data, sheet = x))
+list_all_sites <- 
+  lapply(tab_names[-10], function(x) read_excel(path = x1_data, sheet = x))
 
-str(list_all)
+all_sites <- do.call('rbind',list_all_sites)  # combined all sites
+
+GPS_locations <-     # isolated data frame with GPS locations
+  lapply(tab_names[10], function(x) read_excel(path = x1_data, sheet = x))
+
+sites_combined <- merge(all_sites, GPS_locations, by='Watershed')
+
+sites_combined %>% 
+  ggplot(aes('Sampling date','Conductivity (µS)',  colour = Watershed)) + 
+  geom_line() + 
+  labs(y = "Estimated number of injuries")
+
+
+names(all_sites)
+
 
